257f3cf6107ba0677c2c6846320100b7d2c6c5d4
Joshua Ching <jchingdev@gmail.com>
2016-03-21 21:24:06 -0400

    init project

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //float translation = Input.GetAxis("Vertical") * speed;
+        //float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        float translation = CrossPlatformInputManager.GetAxis("Vertical") * speed;
+        float rotation = CrossPlatformInputManager.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        //transform.Translate(rotation, 0, translation);
+
+        animator.SetBool("isWalking", (translation == 0) ? false : true);
+	}
+}
