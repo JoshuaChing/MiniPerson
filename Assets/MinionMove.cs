54d795236c5bc9d36bccf98969d618ec2f60008e
Joshua Ching <jchingdev@gmail.com>
2016-03-25 16:26:10 -0400

    minion movement

---
+++ Assets/MinionMove.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionMove : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 3f;
+    public float rotationSpeed = 3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveToTarget();
+        RotateToTarget();
+    }
+    
+    void MoveToTarget()
+    {
+        float movement = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, movement);
+    }
+
+    void RotateToTarget()
+    {
+        Vector3 lookPosition = target.position - transform.position;
+        lookPosition.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(lookPosition);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+    }
+}
